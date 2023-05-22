0e424f56141113a6c482c399ff6be6d3bb30ff62
R-Mohnish-Kumar <mohnish694@gmail.com>
2023-05-22 07:30:39 +0530

    Small Update

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,5 @@
 class Constant{
-  static String uri='http://192.168.237.65:3000';
+  static String uri='http://10.0.2.2:3000';
 }
 //http://10.0.2.2:3000 for emulator
+//http://192.168.237.65:3000 for device
